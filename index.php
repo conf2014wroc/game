a14240572d49679855b0147c59aa0183f0841ef5
reverff <reverff@gmail.com>
2014-05-02 05:21:03 +0300

    ...

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>The Game</title>
+<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf8">
+<?php require_once('inc/db_connector.php'); ?>
+</head>
+<body>
+
+   	<div class="main_tyle" >
+			<?php require_once('inc/mainpage_text.txt'); ?>
+	</div>
+
+</body>
+</html>
