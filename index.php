633cec46d7700388cd2b5d584069008b2fbe777b
reverff <reverff@gmail.com>
2014-05-04 04:26:51 +0300

    we have start page

--- index.php
+++ index.php
@@ -6,10 +6,21 @@
 <?php require_once('inc/db_connector.php'); ?>
 </head>
 <body>
-
-   	<div class="main_tyle" >
+	
+    <center>
+		<h1 style="text-height:40;">The Game</h1>
+	
+	   	<div class="whitebox">
 			<?php require_once('inc/mainpage_text.txt'); ?>
-	</div>
+		</div>
+        
+        <a href="#"><img src="img/list.png" /></a>
+	</center>
+    
+    <img src="img/thief.png" width="220" id="pic1" />
+    <img src="img/painter.png" width="240" id="pic2" />
+    <img src="img/student.png" width="240" id="pic3" />
+    <img src="img/museum.png" width="240" id="pic4" />
 
 </body>
 </html>
